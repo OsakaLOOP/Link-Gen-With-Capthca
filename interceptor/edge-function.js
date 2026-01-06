1524c2ff18a042a6f5337bcecb7066cd6f439d28
mywrh <68284076+mywrh@users.noreply.github.com>
2026-01-06 10:27:22 +0800

    #

--- interceptor/edge-function.js
+++ interceptor/edge-function.js
@@ -67,7 +67,7 @@ async function handleRequest(event) {
         
 
         // Access environment variable. In EdgeOne, variables bound to the function are typically global.
-        const SECRET = 'zundamon0721';
+        const SECRET = event.env.JWT_SECRET;
         if (!SECRET) {return new Response(JSON.stringify({ error: "Server Misconfiguration" }), { status: 500, headers: {
             "content-type": "application/json; charset=utf-8",
             "access-control-allow-origin": "*"
